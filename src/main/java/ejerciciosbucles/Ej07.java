1362465b86f2ec243fab4d9c43bb30eb1c381a65
FernandoMarOrt <fernandomartinezortiz003@gmail.com>
2022-10-21 13:44:40 +0200

    7

--- src/main/java/ejerciciosbucles/Ej07.java
+++ src/main/java/ejerciciosbucles/Ej07.java
@@ -1,13 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ejerciciosbucles;
 
-/**
- *
- * @author fer
- */
 public class Ej07 {
-    
+
+    public static void main(String[] args) {
+
+        //Simplimente va sumando a j + 1 cada vez que el bucle da una vuelta
+        // que seria un total de 4 vueltas es decir  j=1 +4 = 5 el resultado
+        int j = 1;
+        for (int i = -13; i <= -10; i++) {
+            System.out.println(j);
+            j++;
+        }
+        System.out.println(j);
+
+    }
+
 }
